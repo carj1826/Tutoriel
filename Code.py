4b56ff99b21ddc5a103e294d3f451699dc1345b8
carj1826 <carj1826@usherbrooke.ca>
2024-03-21 14:44:55 -0400

    Commit file

--- Code.py
+++ Code.py
@@ -1,19 +1,19 @@
 #Création du tuple
-Tuple = (1,2,3,4)
+Tuple = ("Client1","Client2","Client3","Client4")
 
 #Création de la liste
-Liste = [5,6,7,8]
+Liste = ["Client1","Client2","Client3","Client4"]
 
 #Modification d'un élément de la liste
-Liste[0]=10
+Liste[0]="Client5"
 #Afficher le résultat du changement
 print(Liste)
 
 #Tentative de modification d'un élément du tuple
-#Tuple[0]=10 #Cela va créer une erreur; TypeError: 'tuple' object does not support item assignment
+#Tuple[0]="Client5" #Cela va créer une erreur; TypeError: 'tuple' object does not support item assignment
 
 # Accéder aux éléments dans la liste et le tuple
-print("Éléments de la liste :", Liste[0])    # Affichera :10
-print("Éléments du tuple :", Tuple[0])      # Affichera : 1
+print("Éléments de la liste :", Liste[0])    # Affichera :Client5
+print("Éléments du tuple :", Tuple[1])      # Affichera : Client2
 
 #On peut aussi
